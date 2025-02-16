d9dc0b18d986d7d6eb594c811e30423f0657394b
Dells27 <alejandroleitonsegura@gmail.com>
2025-02-16 00:17:43 -0600

    Crud

---
+++ TareaGrupal/TareaGrupal/Controllers/CustomerController.cs
@@ -0,0 +1,135 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TareaGrupal.Data;
+using TareaGrupal.Models;
+
+namespace TareaGrupal.Controllers
+{
+    public class CustomerController : Controller
+    {
+
+        private readonly AppDbContext _context;
+
+        public CustomerController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Principal()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var customer = await _context.customers.ToListAsync();
+            return View(customer);
+
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id, first_name, last_name, email, phone, address")] Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(customer);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(customer);
+        }
+        public async Task<IActionResult> Edit(int id)
+        {
+            var customer = await _context.customers.FindAsync(id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+            return View(customer);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, Customer customer)
+        {
+            if (id != customer.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(customer);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CustomerExists(customer.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(customer);
+        }
+
+        private bool CustomerExists(int id)
+        {
+            return _context.customers.Any(e => e.Id == id);
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            var customer = await _context.customers.FindAsync(id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+            return View(customer);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(int id, Customer customer)
+        {
+            if (id != customer.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Remove(customer);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CustomerExists(customer.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(customer);
+        }
+
+    }
+}
---
+++ TareaGrupal/TareaGrupal/Models/Customer.cs
@@ -0,0 +1,14 @@
+﻿namespace TareaGrupal.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public required string first_name { get; set; }
+
+        public required string last_name { get; set; }
+
+        public required string email { get; set; }
+        public required string phone { get; set; }
+        public required string address { get; set; }
+    }
+}
