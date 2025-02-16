57a4433e8892840841455d4ca9c0db5075fa5327
Carlos Oviedo <carlos.oviedo@smartsmd.com>
2025-02-16 14:55:04 -0600

    Create CRUD for products

---
+++ TareaGrupal/TareaGrupal/Controllers/ProductController.cs
@@ -0,0 +1,120 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TareaGrupal.Data;
+using TareaGrupal.Models;
+namespace TareaGrupal.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly AppDbContext _context;
+        public ProductController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Principal()
+        {
+            return View();
+        }
+        public async Task<IActionResult> Index()
+        {
+            var product = await _context.products.ToListAsync();
+            return View(product);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id, name, description, price, supplier_id")] Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(product);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(product);
+        }
+        public async Task<IActionResult> Edit(int id)
+        {
+            var product = await _context.products.FindAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return View(product);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, Product product)
+        {
+            if (id != product.id)
+            {
+                return NotFound();
+            }
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(product);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (ProductExists(product.id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(product);
+        }
+        private bool ProductExists(int id)
+        {
+            return _context.products.Any(e => e.id == id);
+        }
+        public async Task<IActionResult> Delete(int id)
+        {
+            var product = await _context.products.FindAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return View(product);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Delete(int id, Product product)
+        {
+            if (id != product.id)
+            {
+                return NotFound();
+            }
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Remove(product);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ProductExists(product.id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(product);
+        }
+    }
+}
--- TareaGrupal/TareaGrupal/Data/AppDbContext.cs
+++ TareaGrupal/TareaGrupal/Data/AppDbContext.cs
@@ -8,7 +8,7 @@ namespace TareaGrupal.Data
 
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
-
+        public DbSet<Product> products { get; set; }
         public DbSet<Customer> customers { get; set; }
 
    
